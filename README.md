31cfa51444b4351eed982234e801a7902bb12539
Eduardo Martínez <emartinezr161@outlook.com>
2022-02-15 19:29:02 +0100

    commit inicial

---
+++ README.md
@@ -0,0 +1,18 @@
+<p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400"></a></p>
+
+
+## Sobre Laramusi
+
+Ejemplo hípersencillo de un "spotify" compuesto principalmete por:
+- Albumes, que a su vez están compuestos por
+- Temas
+
+Es un excusa para practicar con Laravel Breeze y luego desplegar en Heroku.
+
+## Álbum
+- Título
+- Autor
+
+## Tema
+- Título
+- Duración
---
+++ database/migrations/2022_02_10_182207_create_temas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\Grammars\Grammar;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Grammar::macro('typeInterval', function () {
+            return 'interval';
+        });
+
+        Schema::create('temas', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->addColumn('interval', 'duracion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('temas');
+    }
+};
